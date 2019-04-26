4365d91527314538b5c24e6161b9025e51e9ddac
GuilleFux <49064286+GuilleFux@users.noreply.github.com>
2019-04-26 13:21:24 +0200

    Update api.jsx

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export default axios.create({
-  baseURL: 'https://mernapi-rest.herokuapp.com'
+  baseURL: 'https://app-mernapi.herokuapp.com'
   //baseURL: 'http://localhost:5000'
 });
